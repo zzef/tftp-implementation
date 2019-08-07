88a89bd4846d9b784f504405682adbdb38302c9b
Joseph Sigbeku <josephsgbku@gmail.com>
2019-08-07 01:30:10 +0100

    short client implementation

--- tftpc.c
+++ tftpc.c
@@ -1,2 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include "sockets.h"
+
+int main(int argc, char** argv) {
+	
+	char* IP_ADDRESS = argv[1];
+	
+	int sockfd = socket(AF_INET, SOCK_DGRAM,0);
+	if (sockfd<0){
+        printf("Error: Failed to create socket - %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Successfully created socket!\n");
+    }
+    
+	send_data(sockfd,IP_ADDRESS,69,"HELLO BOYS AND GIRLS");	
+
+}
