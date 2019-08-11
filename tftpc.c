411702ab5e4ec0a75fd554ee5ee3833f38520e86
Joseph Sigbeku <josephsgbku@gmail.com>
2019-08-12 00:14:14 +0100

    Revert "Now including packet_debug.h library. Uses print_pkt_data() to print packet data."
    This reverts commit 04a56530a91ed7b235231db9604088a70eb6d818.

--- tftpc.c
+++ tftpc.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include "sockets.h"
 #include "tftp_utils.h"
-#include "packet_debug.h"
 
 int main(int argc, char** argv) {
 		
@@ -19,20 +18,15 @@ int main(int argc, char** argv) {
     } else {
         printf("Successfully created socket!\n");
     } 
-
 	char* wrq;
-	char* rrq;
-
-	int size_wrq = bake_rq_pkt('r',"test_file.txt","octet",&wrq);
-	int size_rrq = bake_rq_pkt('w',"test_file.txt","octet",&rrq);
-
-	printf("Sending packet: \"");
-	print_pkt_data(wrq,size_wrq);
-	printf("\" to [%s,%i] (%i bytes)\n",IP_ADDRESS,TFTP_PORT,size_wrq);
-	send_data(sockfd,IP_ADDRESS,TFTP_PORT,wrq,size_wrq);
 	
-	printf("Sending packet: \"");
-	print_pkt_data(rrq,size_rrq);
-	printf("\" to [%s,%i] (%i bytes)\n",IP_ADDRESS,TFTP_PORT,size_rrq);
-	send_data(sockfd,IP_ADDRESS,TFTP_PORT,rrq,size_rrq);
+	printf("yooo0\n");
+
+	int size = bake_wrq_pkt("test_file.txt","octet",&wrq);
+	
+	printf("> %s\n",wrq);
+	
+	send_data(sockfd,IP_ADDRESS,TFTP_PORT,wrq,size);
+	
+	printf("yooo\n");
 }
