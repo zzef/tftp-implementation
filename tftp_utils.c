6fd865d6b69a13bcde55e8752387a5107e64a7ba
Joseph Sigbeku <josephsgbku@gmail.com>
2019-08-11 23:58:30 +0100

    bake_rq_pkt() now takes the type of packet too. Whether RRQ or WRQ. The only difference between the two types of packets is the opcode so a general function was good for this

--- tftp_utils.c
+++ tftp_utils.c
@@ -1,8 +1,26 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-char* concat(char* a, int size_a,
-			 char* b, int size_b, 
+/*
+
+	Opcodes
+	-------
+
+	opcode   operation
+	  1      Read Request (RRQ)
+	  2      Write Request (WRQ)
+	  3      Data (DATA)
+      4      Acknowledgement (ACK)
+      5  	 Error (ERROR)
+
+
+*/
+
+
+//This function concatenates characters
+char* concat(const char* a, int size_a,
+			 const char* b, int size_b, 
 		 	 int* len) {
 
 	char* final = malloc(size_a+size_b);
@@ -12,15 +30,34 @@ char* concat(char* a, int size_a,
 	return final;
 }
 
-int bake_wrq_pkt(char* file_name, 
-					char* mode, 
+int bake_rq_pkt(const char rq, const char* file_name, 
+					const char* mode, 
 					char** wrq){
 	
+/*
+
+
+	RRQ/WRQ Packets
+	--------------
+
+	   2 bytes     string     1 byte   string  1 byte
+	 -------------------------------------------------
+	|  opcode  |  file name  |   0   |  mode  |   0   |                   
+	 -------------------------------------------------
+	
+
+*/
+
 	int length=0;
-	*wrq = "\x00\x02"; 
+	switch(rq){
+		case('r'): *wrq = "\0\1"; break;
+		case('w'): *wrq = "\0\2"; break;
+	}	
 	*wrq = concat(*wrq,2,file_name,strlen(file_name)+1,&length);
 	*wrq = concat(*wrq,length,mode,strlen(mode)+1,&length);
-	printf("%i\n",length);
 
 	return length;
 }
+
+
+
