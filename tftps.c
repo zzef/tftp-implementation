bdd127551b48aa84d7ed4971bd837243abd8b51d
Joseph Sigbeku <josephsgbku@gmail.com>
2019-08-15 04:08:31 +0100

    server now responds to wrq/rrq packets with an acknowledgement packet

--- tftps.c
+++ tftps.c
@@ -5,10 +5,13 @@
 #include <errno.h>
 #include <string.h>
 #include "sockets.h"
-#include "packet_debug.h" 
+#include "packet_debug.h"
+#include "tftp_utils.h" 
 
 int main(int argc, char** argv) {
    	 
+	char* IP_ADDRESS = argv[1];
+
     int sockfd = socket(AF_INET,SOCK_DGRAM,0);  
     if (sockfd<0){
         printf("Error: Failed to create socket - %s\n",strerror(errno));
@@ -17,14 +20,14 @@ int main(int argc, char** argv) {
         printf("Successfully created socket!\n");
     }
     
-    if (bind_socket(sockfd,NULL,PORT)<0) {
+    if (bind_socket(sockfd,IP_ADDRESS,TFTP_PORT)<0) {
         printf("Error: Failed to bind socket - %s\n",strerror(errno));
         exit(EXIT_FAILURE);
     } else {
         printf("Successfully binded socket!\n");
     }
     
-    printf("listening on port %i\n",PORT);
+    printf("listening on port %i\n",TFTP_PORT);
     while(1) {
         
         struct packet* pckt = malloc(sizeof(struct packet));
@@ -32,19 +35,21 @@ int main(int argc, char** argv) {
         if(receive(sockfd,pckt)<0) {
             printf("Failed to receive packet - %s\n",strerror(errno));
         }
-        else {
+        else {		
 			printf("Recieved packet: \"");
 			print_pkt_data(pckt->data,pckt->data_len);
 			printf("\" from [%s,%i] (%li bytes)\n",
-					inet_ntoa(pckt->ip_addr),
+					pckt->ip_addr,
 					pckt->port,
 					pckt->data_len
 					);
 	
-	
+			unsigned char* res = (unsigned char*) pckt->data;
+			if (*res=='\0'&&*(res+1)=='\2') {
+				char *ack_pkt;	
+				int size_ack = bake_ack_pkt(0,&ack_pkt);	
+				send_data(sockfd,pckt->ip_addr,pckt->port,ack_pkt,size_ack);
+			}	
         }
-        
-        
     }
-    
 }
